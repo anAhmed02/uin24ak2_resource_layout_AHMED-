90dede51dfa1ae6ae185bee16ba290217dcedb5c
anAhmed02 <111729831+anAhmed02@users.noreply.github.com>
2024-01-26 01:49:56 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,12 @@
+//
 function showResource(category) {
     const main = document.querySelector('#container');
     main.innerHTML = '';
-
+    // finne ressurser som passe de valgte kategoriene 
     const resource = resources.find(r => r.category.toLowerCase() === category.toLowerCase());
     if (resource) {
         const article = document.createElement('article');
+        //denne koden lage en boks med informasjon om ressursene og viser den på nettsiden
         article.innerHTML = `
             <h1>${resource.category}</h1>
             <p>${resource.text}</p>
@@ -13,12 +15,14 @@ function showResource(category) {
         main.appendChild(article);
     }
 }
-
+//Gjør at hele siden lastes opp på likt isteden for at du ser en helt hvit side før alt lastes inn
 document.addEventListener('DOMContentLoaded', () => {
     showResource('HTML');
+    //denne koden gjør at når du trykker på linkene blir du send rett inn istedenfor å åpne nye faner  
     document.querySelectorAll('nav a').forEach(link => {
         link.addEventListener('click', (event) => {
             event.preventDefault();
+            //vise ressursen basert på lenketeksten
             showResource(event.target.textContent);
         });
     });
